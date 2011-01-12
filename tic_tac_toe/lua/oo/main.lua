5566550aa6901f105da50c8abcf16150eb7b43e1
Maurício Szabo <mauricio.szabo@gmail.com>
2011-01-12 10:52:57 -0200

    Terminado o código em Lua.

--- tic_tac_toe/lua/oo/main.lua
+++ tic_tac_toe/lua/oo/main.lua
@@ -1,31 +1,33 @@
 require("tabuleiro")
 
 Main = {
-  tabuleiro = Tabuleiro
+    tabuleiro = Tabuleiro
 }
 
 function Main:jogar()
-  while not self.tabuleiro:jogo_acabou() do
+    while not self.tabuleiro:jogo_acabou() do
+        self.tabuleiro:imprimir()
+        self:processar_jogada()
+    end
     self.tabuleiro:imprimir()
-    self:processar_jogada()
-  end
-  print()
-  print()
-  print(tabuleiro:mensagem_final())
+    print()
+    print()
+    print(self.tabuleiro:mensagem_final())
 end
 
 function Main:processar_jogada()
-  local jogada=io.read()
-  x, y = jogada:match("([ABC])([123])")
-  if(not x) then return self:erro_jogada() end
-  x = x:byte() - 64
-  if not self.tabuleiro:preencher(x, y) then
-    print("Quadrado já está preenchido!")
-  end
+    local jogada=io.read()
+    x, y = jogada:match("([ABC])([123])")
+    if(not x) then return self:erro_jogada() end
+    x = x:byte() - 64
+    y = y - 0
+    if not self.tabuleiro:preencher(x, y) then
+        print("Quadrado já está preenchido!")
+    end
 end
 
 function Main:erro_jogada()
-  print("Erro na jogada: jogada deve ser A1, A2, B2, etc.")
+    print("Erro na jogada: jogada deve ser A1, A2, B2, etc.")
 end
 
 Main:jogar()
--- tic_tac_toe/lua/oo/tabuleiro.lua
+++ tic_tac_toe/lua/oo/tabuleiro.lua
@@ -1,80 +1,96 @@
 local mensagem_final
 
 Tabuleiro = {
-  '     A   B   C ',
-  '  1    |   |   ',
-  '    ---+---+---',
-  '  2    |   |   ',
-  '    ---+---+---',
-  '  3    |   |   ',
-  jogador = 'X',
-  matriz = { { nil, nil, nil}, { nil, nil, nil}, { nil, nil, nil} }
+    '     A   B   C ',
+    '  1    |   |   ',
+    '    ---+---+---',
+    '  2    |   |   ',
+    '    ---+---+---',
+    '  3    |   |   ',
+    jogador = 'X',
+    matriz = { { false, false, false}, { false, false, false}, { false, false, false } }
 }
 
 function Tabuleiro:imprimir()
-  for i, v in ipairs(self) do print(v) end
-  print("\nJogador " .. self.jogador .. ", sua jogada? ")
+    for i, v in ipairs(self) do print(v) end
+    io.write("\nJogador " .. self.jogador .. ", sua jogada? ")
 end
 
 function Tabuleiro:preencher(x, y)
-  if self.matriz[x][y] then return false end
-  self.matriz[x][y] = self.jogador
-  y = y * 2
-  x = x * 4
-  self[y] = self[y]:sub(1, x+1) .. self.jogador .. self[y]:sub(x+3)
-  if self.jogador == "X" then 
-    self.jogador = "O"
-  else 
-    self.jogador = "X"
-  end
-  return true
+    if self.matriz[x][y] then return false end
+    self.matriz[x][y] = self.jogador
+    y = y * 2
+    x = x * 4
+    self[y] = self[y]:sub(1, x+1) .. self.jogador .. self[y]:sub(x+3)
+    if self.jogador == "X" then 
+        self.jogador = "O"
+    else 
+        self.jogador = "X"
+    end
+    return true
 end
 
 function Tabuleiro:jogo_acabou()
-  self:mensagem_final()
+    return self:mensagem_final()
 end
 
 function Tabuleiro:mensagem_final()
-  if not mensagem_final then
-    h = self:horizontais_ou_verticais_iguais()
-    print(h)
-    --h ||= diagonais_iguais
-    if h then
-      mensagem_final = h .. " Venceu!"
-    --elsif todos_preenchidos?
-      --"Deu velha!"
+    if not mensagem_final then
+        h = self:horizontais_ou_verticais_iguais()
+        h = h or self:diagonais_iguais()
+        if h then
+            mensagem_final = h .. " Venceu!"
+        elseif self:todos_preenchidos() then
+            mensagem_final = "Deu velha!"
+        end
     end
-   
-  end
-  return mensagem_final
+    return mensagem_final
+end
+
+local iguais = function(matriz)
+    elemento = matriz[1]
+    for i = 2, 3, 1 do
+        if elemento ~= matriz[i] then return nil end
+    end
+    return elemento
+end
+
+local transpose = function(matriz)
+    transposta = { {}, {}, {} }
+    for x = 1, #matriz, 1 do
+        for y = 1, #matriz[x], 1 do
+            transposta[y][x] = matriz[x][y]
+        end
+    end
+    return transposta
 end
 
 function Tabuleiro:horizontais_ou_verticais_iguais()
-  local iguais = function(matriz)
-    elemento = matriz[1]
-    for i = 2, 3, 1 do
-      if not elemento == matriz[i] then return nil end
+    for k, m in ipairs(self.matriz) do
+        local elemento = iguais(m)
+        if elemento then return elemento end
     end
-    return elemento
-  end
 
-  for k, m in ipairs(self.matriz) do
-    print("Matriz")
-    print(k)
-    print(m[1])
-    print(m[2])
-    print(m[3])
-    print()
-    local elemento = iguais(m)
-    print("WOW")
-    print(elemento)
-    if elemento then return elemento end
-  end
-  --matriz = @matriz.transpose
-  --3.times do |i|
-  --  next unless matriz[i].all?
-  --  elementos = matriz[i].uniq
-  --  return elementos.first if elementos.size == 1
-  --end
-  return nil
+    matriz = transpose(self.matriz)
+    for k, m in ipairs(matriz) do
+        local elemento = iguais(m)
+        if elemento then return elemento end
+    end
+    return nil
+end
+
+function Tabuleiro:diagonais_iguais()
+    matriz = self.matriz
+    if(matriz[1][1] == matriz[2][2] and matriz[2][2] == matriz[3][3]) then return matriz[1][1] end
+    if(matriz[1][3] == matriz[2][2] and matriz[2][2] == matriz[3][1]) then return matriz[1][3] end
+    return nil;
+end
+
+function Tabuleiro:todos_preenchidos()
+    for k, m1 in ipairs(self.matriz) do
+        for k2, e in ipairs(m1) do
+            if(not e) then return false end
+        end
+    end
+    return true
 end
