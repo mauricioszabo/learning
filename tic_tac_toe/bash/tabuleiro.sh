d1c1a8f54c35e60f259e39c0583d2c332d1a8b80
Mauricio Szabo <mauricio.szabo@gmail.com>
2011-02-11 00:11:41 -0200

    Terminada a versão em Shell Script.

--- tic_tac_toe/bash/tabuleiro.sh
+++ tic_tac_toe/bash/tabuleiro.sh
@@ -17,13 +17,13 @@ imprimir_tabuleiro() {
 preencher_tabuleiro() {
     x=$1
     y=$2
-    if [ ${matriz[$x$y]} ]; then return 0; fi
+    [ ${matriz[$x$y]} ] && return 0
     matriz[$x$y]=$jogador
     y=$(($y * 2 + 1))
     x=$(($x * 4 + 5))
     t=${tabuleiro[$y]}
     tabuleiro[$y]="${t:0:$x}$jogador${t:$((x+1))}"
-    if [ $jogador == "X" ]; then jogador="Y"; else jogador="X"; fi
+    [ $jogador == "X" ] && jogador="Y" || jogador="X"
     return 1
 }
 
@@ -38,9 +38,14 @@ jogo_acabou() {
 mensagem_final() {
     if [ ! $m_mensagem_final ]; then
         h=$(horizontais_ou_verticais_iguais)
-        if [ "$h" == "" ]; then h=$(diagonais_iguais); fi
+        [ "$h" == "" ] && h=$(diagonais_iguais)
         if [ "$h" != "" ]; then 
             m_mensagem_final="$h Venceu!"
+        else
+            todos_preenchidos
+            if [ $? == 1 ]; then
+                m_mensagem_final="Deu velha!"
+            fi
         fi
     fi
 
@@ -85,18 +90,16 @@ iguais() {
 }
 
 diagonais_iguais() {
-    if [ ! ${matriz[11]} ]; then return; fi
+    [ ! ${matriz[11]} ] && return
     [ "${matriz[00]}" == "${matriz[11]}" ] && [ "${matriz[22]}" == "${matriz[11]}" ] && echo "${matriz[00]}"
     [ "${matriz[02]}" == "${matriz[11]}" ] && [ "${matriz[20]}" == "${matriz[11]}" ] && echo "${matriz[02]}"
 }
 
-#int todos_preenchidos(Tabuleiro *tabuleiro) {
-#    int x, y;
-#    for(x = 0; x < 3; x++) {
-#        for(y = 0; y < 3; y++) {
-#            if(!tabuleiro->matriz[x][y]) return FALSE;
-#        }
-#    }
-#    return TRUE;
-#}
+todos_preenchidos() {
+    for x in {0..2}; do
+        for y in {0..2}; do
+            [ ! ${matriz[$x$y]} ] && return 0
+        done
+    done
+}
 
