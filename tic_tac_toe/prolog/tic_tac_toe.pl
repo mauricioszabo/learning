8fae06c8ef50ca04517867ebd067746c7dfaae27
Maurício Szabo <mauricio.szabo@gmail.com>
2014-10-08 13:47:08 -0300

    Recursive prints, and corrections on diagonal winning.

--- tic_tac_toe/prolog/tic_tac_toe.pl
+++ tic_tac_toe/prolog/tic_tac_toe.pl
@@ -1,12 +1,11 @@
 % prolog
 
 create_board :-
-    member(Col, ['C', 'B', 'A']),
-    member(Row, ['3', '2', '1']),
+    member(Col, ['A', 'B', 'C']),
+    member(Row, ['1', '2', '3']),
     asserta(board(Col, Row, empty)),
     fail.
 create_board.
-:- create_board.
 
 % Winning rules:
 % Horizontal and Vertical
@@ -14,7 +13,7 @@ won(Player) :- board(X, '1', Player), board(X, '2', Player), board(X, '3', Playe
 won(Player) :- board('A', Y, Player), board('B', Y, Player), board('C', Y, Player).
 % Diagonal
 won(Player) :- board('A', '1', Player), board('B', '2', Player), board('C', '3', Player).
-won(Player) :- board('C', '1', Player), board('B', '2', Player), board('A', '1', Player).
+won(Player) :- board('C', '1', Player), board('B', '2', Player), board('A', '3', Player).
 
 next_player('X', 'O').
 next_player('O', 'X').
@@ -33,39 +32,30 @@ prompt(_) :- fail.
 
 print_board :-
     tab(8), write('   A   B   C'), nl,
-    tab(8), write('1 '), print_row('1'), nl,
-    tab(10), write('---+---+---'), nl,
-    tab(8), write('2 '), print_row('2'), nl,
-    tab(10), write('---+---+---'), nl,
-    tab(8), write('3 '), print_row('3'), nl, nl.
+    print_row(['1', '2', '3']), !, nl.
 
-print_row(Row) :-
+print_row([Row]) :-
+    tab(8), write(Row), tab(1), 
     board('A', Row, C1), print_cell(C1), write('|'),
     board('B', Row, C2), print_cell(C2), write('|'),
-    board('C', Row, C3), print_cell(C3).
+    board('C', Row, C3), print_cell(C3), nl.
 
-print_cell(empty) :- 
-    write('   ').
-print_cell(Player) :- 
-    Player \= empty,
-    write(' '), write(Player), write(' ').
+print_row([Head | Tail]) :-
+    print_row([Head]), tab(10), write('---+---+---'), nl, print_row(Tail).
 
-play(Player) :-
-    print_board, prompt(Player), read_input(Player),
-    next_step(Player).
+print_cell(empty) :- tab(3).
+print_cell(Player) :- Player \= empty, tab(1), write(Player), tab(1).
+
+play(Player) :- print_board, prompt(Player), read_input(Player), next_step(Player).
 play(Player) :- nl, play(Player).
 
-next_step(Player) :-
-    won(Player), print_board,
-    write(Player), write(' venceu!'), nl.
+next_step(Player) :- won(Player), print_board, write(Player), write(' venceu!'), nl.
 
-next_step(Player) :-
-    board(_, _, empty),
-    next_player(Player, Next), play(Next).
+next_step(Player) :- board(_, _, empty), next_player(Player, Next), play(Next).
 
 next_step(_) :- print_board, write('Deu velha!'), nl.
 
-main :- play('X'), halt.
+main :- create_board, play('X'), halt.
 
 % Comment this line if you don't want to automatically run the program.
 :- main.
