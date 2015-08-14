a67e8148b3ebe76bf7c96275e45c28deb21363d1
Maurício Szabo <mauricio.szabo@gmail.com>
2015-08-13 23:55:22 -0300

    Added more code.

--- tic_tac_toe/clojure/board.clj
+++ tic_tac_toe/clojure/board.clj
@@ -1,6 +1,21 @@
 (def empty-board (fn []
   (let
-    [row, (vector " " " " " ")]
-    (vector row row row))))
+    [row, [" " " " " "]]
+    [row row row])))
+
+(def won (fn [board]
+  (or
+    (same-horiz board)
+    (same-vert board)
+    (same-diag1 board)
+    (same-diag2 board))))
+
+(def same-horiz (fn [board]
+  (first
+    (filter
+      (fn [row]
+        (and (= (count (set row)) 1) (not (= (first row) " "))))
+      board))))
+
 
 (nth (empty-board) 1)
