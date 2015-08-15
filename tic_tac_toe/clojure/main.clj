73c4cb1a591e6418fc57e84dce2144dd3348e2c8
Maurício Szabo <mauricio.szabo@gmail.com>
2015-08-15 19:26:55 -0300

    Added play game code.

---
+++ tic_tac_toe/clojure/main.clj
@@ -0,0 +1,52 @@
+(load "board")
+
+(defn print-board [board]
+  (do
+    (def sep "   --+---+--\n")
+    (def j clojure.string/join)
+    (def lines (map-indexed
+      (fn [i row] (str (+ i 1) "  " (j " | " row) "\n"))
+      board))
+    (println "\n   A   B   C")
+    (println (j sep lines))
+    (println "")))
+
+(declare play)
+
+(defn invalid-movement [board player]
+  (do
+    (println "Invalid move. Movements must be like 'A1', 'B2', etc.\n")
+    (play board player)))
+
+(defn valid-movement [board player colMatch rowMatch]
+  (do
+    (def col (.indexOf ["A" "B" "C"] colMatch))
+    (def row (- (Integer/parseInt rowMatch) 1))
+    (def new-player (if (= player "X") "O" "X"))
+    (def new-board (assoc board row (assoc (nth board row) col player)))
+    (play new-board new-player)))
+
+(defn treat-movement [board player]
+  (let [[_ colMatch rowMatch]
+        (re-matches #"([ABC])\-?([123])" (read-line))]
+    (if colMatch
+      (valid-movement board player colMatch rowMatch)
+      (invalid-movement board player))))
+
+(defn next-turn [board player] (do
+  (print "Player" player ", your move: ")
+  (flush)
+  (treat-movement board player)))
+
+(defn won-game [victorious] (do
+  (println "Player" victorious "won!\n")))
+
+(defn play [board player] (do
+  (print-board board)
+  (def victorious (who-won? board))
+
+  (if victorious
+    (won-game victorious)
+    (next-turn board player))))
+
+(play empty-board "X")
