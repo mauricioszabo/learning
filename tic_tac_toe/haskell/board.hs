4f1cb16b5c0770db64a4b41a81bcbcf2dff2a180
Maurício Szabo <mauricio.szabo@gmail.com>
2017-11-07 20:24:42 -0200

    Unit testing Haskell board

---
+++ tic_tac_toe/haskell/board.hs
@@ -0,0 +1,25 @@
+module Board where
+import Data.Vector as Vector
+import Data.List as List
+type Board = Vector (Vector Mark)
+
+data Mark = Empty | X | O deriving (Eq, Show)
+
+empty = Vector.replicate 3 $ Vector.replicate 3 Empty
+
+showBoard board =
+  let rowsAsList = Vector.map Vector.toList board
+      boardAsList = Vector.toList rowsAsList
+      rows = List.map showRow boardAsList
+      -- full
+      -- withHeader = "
+      in List.intercalate "\n---+---+---\n" rows
+  where
+    showRow row = ' ' : ' ' : (List.intercalate " | " $ List.map toString row)
+    toString Empty = " "
+    toString player = show player
+
+update :: Board -> (Int, Int) -> Mark -> Board
+update board (row, col) mark =
+  let inner = board ! row // [(col, mark)]
+    in board // [(row, inner)]
