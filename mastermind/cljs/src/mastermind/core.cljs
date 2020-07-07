0a5b44709604669e87a3a53d7e8d78562838a590
Maurício Szabo <mauricio.szabo@gmail.com>
2020-07-07 19:09:00 -0300

    Win and Lose implemented

--- mastermind/cljs/src/mastermind/core.cljs
+++ mastermind/cljs/src/mastermind/core.cljs
@@ -6,15 +6,20 @@
 
 (defonce state (r/atom {}))
 
-(def ^:private colors {:red "rgb(223, 41, 53)"
-                       :green "rgb(12, 131, 70)"
-                       :blue "#00A"
-                       :yellow "rgb(255, 212, 0)"
-                       :pink "rgb(246, 91, 227)"
-                       :gray "#A5A5A5"})
+(def ^:private get-color {:red "rgb(223, 41, 53)"
+                          :green "rgb(12, 131, 70)"
+                          :blue "#00A"
+                          :yellow "rgb(255, 212, 0)"
+                          :pink "rgb(246, 91, 227)"
+                          :gray "#A5A5A5"})
 
-(defn- color-circle [idx color]
-  [:div.circle {:key idx :style {:background-color (colors color)}}])
+(defn- color-circle
+  ([idx color]
+   [:div.circle {:key idx :style {:background-color (get-color color)}}])
+  ([idx color size]
+   [:div.circle {:key idx :style {:background-color (get-color color)
+                                  :width size
+                                  :height size}}]))
 
 (defn- old-guess [colors {:keys [result guess]}]
   (let [{:keys [black white]} result]
@@ -26,7 +31,7 @@
   ^{:key color}
   [:> Button {:variant "contained"
               :disable-elevation true
-              :style {:width "6em" :height "2.5em" :background-color (colors color)}
+              :style {:width "6em" :height "2.5em" :background-color (get-color color)}
               :on-click (fn [_]
                           (swap! state update :current-guess
                                  #(cond-> % (< (count %) 4) (conj color))))}])
@@ -34,36 +39,42 @@
 (declare new-game!)
 (defn- send-guess! [state]
   (let [current-guess (:current-guess @state)
-        result (rules/compare-guess (:colors @state) current-guess)]
-    (when (and (not (:win? @state))
-               (= 4 (count current-guess)))
+        colors (:colors @state)
+        result (rules/compare-guess colors current-guess)]
+    (when (and (= 4 (count current-guess)))
       (swap! state
              #(-> %
-                  (assoc :current-guess [] :win? (-> result :black (= 4)))
+                  (assoc :current-guess [] :win? (= current-guess colors))
                   (update :old-guesses conj {:result result :guess current-guess}))))))
 
 (defn- index [state]
-  [:> Container {:style {:margin-top "2em" :margin-bottom "2em"}}
-   [:> Paper
-    [:> Container {:style {:display "flex" :flex-direction "column" :align-items "center"}}
-     [:h1 "MASTERMIND REAGENT"]
-     [:> Button {:variant "outlined" :on-click new-game!} "New Game"]
-     [:div.cols (map color-circle (range) (:colors @state))]
-     [:div.cols {:style {:margin-top "1.5em"}} [:b "Guesses"]]
-     (let [colors (:colors @state)]
+  (let [{:keys [old-guesses colors win?]} @state]
+    [:> Container {:style {:margin-top "2em" :margin-bottom "2em"}}
+     [:> Paper
+      [:> Container {:style {:display "flex" :flex-direction "column" :align-items "center"}}
+       [:h1 "MASTERMIND REAGENT"]
+       [:> Button {:variant "outlined" :on-click new-game!} "New Game"]
+       [:div.cols
+        (if win?
+          (map #(color-circle %1 %2 "3em") (range 4) colors)
+          (map #(color-circle % :black "3em") (range 4)))]
+       [:div.cols {:style {:margin-top "1.5em"}} [:b "Guesses"]]
        [:div.rows (map (fn [g i] ^{:key i} [old-guess colors g])
-                       (:old-guesses @state)
-                       (range))])
+                       old-guesses
+                       (range))]
 
-     [:div.cols {:style {:margin-top "5em" :height "3em"}}
-      (map color-circle (range) (:current-guess @state))]
-     [:div.cols {:style {:width "35em" :justify-content "space-between"}}
-      (map #(buttons state %) (keys colors))]
-     [:> ButtonGroup {:style {:margin-top "1em" :margin-bottom "1em"}}
-      [:> Button {:on-click (fn [_] (send-guess! state))}
-       "Guess"]
-      [:> Button {:on-click (fn [_] (swap! state assoc :current-guess []))}
-       "Reset"]]]]])
+       [:div.cols {:style {:margin-top "5em" :height "3em"}}
+        (map color-circle (range) (:current-guess @state))]
+       [:div.cols {:style {:width "35em" :justify-content "space-between"}}
+        (map #(buttons state %) (keys get-color))]
+       (cond
+         win? [:h2 "You have won the game!"]
+         (= 10 (count old-guesses)) [:h2 "You have LOST the game! Looser!!!"]
+         :else [:> ButtonGroup {:style {:margin-top "1em" :margin-bottom "1em"}}
+                [:> Button {:on-click (fn [_] (send-guess! state))}
+                 "Guess"]
+                [:> Button {:on-click (fn [_] (swap! state assoc :current-guess []))}
+                 "Reset"]])]]]))
 
 (defn ^:dev/after-load render []
   (r-dom/render [index state] (. js/document querySelector "#app")))
@@ -73,7 +84,7 @@
     (swap! state assoc
            :colors colors
            :old-guesses []
-           :win? false
+           :win? nil
            :current-guess [])))
 
 (defn main []
