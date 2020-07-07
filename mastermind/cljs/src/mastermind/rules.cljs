ba6c89398806a4519e5073cdeb51ba6a33586703
Maurício Szabo <mauricio.szabo@gmail.com>
2020-07-07 18:34:44 -0300

    Fixed colors

--- mastermind/cljs/src/mastermind/rules.cljs
+++ mastermind/cljs/src/mastermind/rules.cljs
@@ -1,12 +1,12 @@
 (ns mastermind.rules
   (:require [clojure.test :as t]))
 
-(def colors (->> [:red :green :blue :yellow :pink :black]
+(def colors (->> [:red :green :blue :yellow :pink :gray]
                  cycle
                  (take (* 4 6))))
 
 (defn shuffle-colors []
-  (->> colors shuffle (take 4)))
+  (->> colors shuffle (take 4) vec))
 (t/deftest shuffle-colors-test
   (t/is (= 4 (count (shuffle-colors)))))
 
@@ -24,14 +24,14 @@
   (t/testing "guess everything wrong"
     (t/is (= {:white 0 :black 0}
              (compare-guess [:red :green :blue :yellow]
-                            [:pink :black :pink :black]))))
+                            [:pink :gray :pink :gray]))))
 
   (t/testing "guess something on the right place"
     (t/is (= {:white 0 :black 1}
              (compare-guess [:red :green :blue :yellow]
-                            [:red :red :black :pink]))))
+                            [:red :red :gray :pink]))))
 
   (t/testing "guess something on the right place, and some on wrong"
     (t/is (= {:white 2 :black 1}
              (compare-guess [:red :blue :red :red]
-                            [:red :red :black :blue])))))
+                            [:red :red :gray :blue])))))
